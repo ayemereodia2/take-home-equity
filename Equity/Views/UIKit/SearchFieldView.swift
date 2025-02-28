8526498afd5c9e124055e344e69d6e7abe5a05a0
ayemereodia2 <odiadavid2@gmail.com>
2025-02-27 23:46:40 -0400

    fixed auto layout errors and gitignore file

---
+++ Equity/Views/UIKit/SearchFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchFieldView.swift
+//  Equity
+//
+//  Created by ANDELA on 27/02/2025.
+//
+
+import UIKit
+
+class SearchFieldView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Equity/Views/UIKit/TopSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  TopSearchView.swift
+//  Equity
+//
+//  Created by ANDELA on 27/02/2025.
+//
+
+import UIKit
+
+class TopSearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
